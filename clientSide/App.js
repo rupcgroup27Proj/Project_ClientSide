44c27bfaf8b596afb0ba3a23d34cd691bebe0ad2
Barel077 <125794992+rupcgroup27Proj@users.noreply.github.com>
2023-02-22 12:04:45 +0200

    init

--- clientSide/App.js
+++ clientSide/App.js
@@ -1,12 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const Stack = createNativeStackNavigator();
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/*This how to create a screen to navigate into:*/}
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }}/>
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
