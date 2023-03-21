c57aefe9bb1d1ef2fbb742c66e64d4395cabcbb6
Barel077 <125794992+rupcgroup27Proj@users.noreply.github.com>
2023-03-21 15:42:42 +0200

    a

--- clientSide/App.js
+++ clientSide/App.js
@@ -1,37 +1,71 @@
-//import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet } from "react-native";
-//import { SafeAreaProvider } from "react-native-safe-area-context";
-//import { createDrawerNavigator } from "@react-navigation/drawer";
-//import AsyncStorage from "@react-native-async-storage/async-storage";
-//import { useEffect, useState } from "react";
-import NewPost from "./Screens/StudentScreens/SocialCloud/NewPost";
-import SocialFeed from "./Screens/StudentScreens/SocialCloud/SocialFeed";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-const Stack = createNativeStackNavigator();
-//hi
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+
+//Context
+import UserProvider from './Components/Contexts/UserContext'
+
+//Navs
+import AdminNav from './Components/Navs/AdminNav';
+import StudentDrawer from './Components/Navs/StudentDrawer'
+import GuideDrawer from './Components/Navs/GuideDrawer';
+import TeacherDrawer from './Components/Navs/TeacherDrawer';
+
+//LoginScreen
+import LoginScreen from './Screens/SharedScreens/Login/LoginScreen';
+
 export default function App() {
-  return (
-    // <SafeAreaProvider>
-    // <NewPost></NewPost>
-    // </SafeAreaProvider>
 
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="NewPost">
-        <Stack.Screen name="SocialFeed" component={SocialFeed} />
-        <Stack.Screen name="NewPost" component={NewPost}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+  const [user, setUser] = useState({})
+
+  const Tab = createMaterialBottomTabNavigator();
+  const Drawer = createDrawerNavigator();
+
+
+  async function GetLoggedUser() {
+    // const loggedUser = await AsyncStorage.getItem('User');
+    // setUser(JSON.parse(loggedUser))
+    setUser({ Type: 'Student', UserId: 123123 })
+  }
+
+  useEffect(() => {
+    GetLoggedUser();
+  }, [])
+
+
+
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+
+
+        <LoginScreen></LoginScreen>
+
+        {/*     
+        {user.Type == 'Admin' && <AdminNav />}
+        {user.Type == 'Student' && <StudentDrawer />}
+        {user.Type == 'Teacher' && <TeacherDrawer />}
+        {user.Type == 'Guide' && <GuideDrawer />} 
+        */}
+
+
+
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
