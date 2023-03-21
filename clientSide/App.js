4936b91dbe4d58477368af33dd30e1b6df46b3b3
ShaharGH <shahar23oved@gmail.com>
2023-03-21 15:22:16 +0200

    UPDATE

--- clientSide/App.js
+++ clientSide/App.js
@@ -1,68 +1,37 @@
-
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect, useState } from 'react';
-
-//Navs
-import AdminNav from './Components/Navs/AdminNav';
-import StudentNav from './Components/Navs/StudentDrawer'
+//import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
+//import { SafeAreaProvider } from "react-native-safe-area-context";
+//import { createDrawerNavigator } from "@react-navigation/drawer";
+//import AsyncStorage from "@react-native-async-storage/async-storage";
+//import { useEffect, useState } from "react";
+import NewPost from "./Screens/StudentScreens/SocialCloud/NewPost";
+import SocialFeed from "./Screens/StudentScreens/SocialCloud/SocialFeed";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 
-
-
-
-
-
-
-
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-  const [user, setUser] = useState({})
-
-  const Tab = createMaterialBottomTabNavigator();
-  const Drawer = createDrawerNavigator();
-
-
-  async function GetLoggedUser() {
-    // const loggedUser = await AsyncStorage.getItem('User');
-    // setUser(JSON.parse(loggedUser))
-    setUser({ Type: 'Student', UserId: 123123 })
-  }
-
-  useEffect(() => {
-    GetLoggedUser();
-  }, [])
-
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
+    // <SafeAreaProvider>
+    // <NewPost></NewPost>
+    // </SafeAreaProvider>
 
-        {user.Type == 'Admin' && <AdminNav/>
-         
-          //   <Drawer.Screen name="mmmmmm" component={TabNav} />
-
-        }
-
-        {user.Type == 'Student' && <StudentNav/>}
-        
- 
-
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="NewPost">
+        <Stack.Screen name="SocialFeed" component={SocialFeed} />
+        <Stack.Screen name="NewPost" component={NewPost}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- clientSide/Screens/StudentScreens/SocialCloud/Styles.js
+++ clientSide/Screens/StudentScreens/SocialCloud/Styles.js
@@ -1,3 +1,40 @@
 import { StyleSheet } from "react-native";
 
-export const Styles = StyleSheet.create({})
+export const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#307ecc",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#307ecc",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 15,
+  },
+  newPostImage: {
+    width: 350,
+    height: 350,
+    marginTop: 35,
+    marginBottom: 35,
+    marginLeft: 25,
+    marginRight: 35,
+    resizeMode: "contain",
+  },
+  newPostContainer: {
+    backgroundColor: "#666",
+    height: "100%",
+  },
+  iconCamera: {
+    backgroundColor: "#000",
+    marginTop: 50,
+    marginLeft: 85,
+    marginRight: 85,
+  },
+});
